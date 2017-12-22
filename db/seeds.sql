19576e20b9dafa80f5359efbf69e1b0c8c7a1366
kferny1 <kferny1@gmail.com>
2017-12-22 14:54:24 -0700

    first commit

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers(burger_name, devoured, date) VALUES ("Cheeseburger", false, "2017-09-04 09:20:00");
+  
+INSERT INTO burgers(burger_name, devoured, date) VALUES ("Big Mac", false, "2017-09-04 09:20:01");
+
+INSERT INTO burgers(burger_name, devoured, date) VALUES ("Veggie burger", false, "2017-09-05 16:20:01");
+
+INSERT INTO burgers(burger_name, devoured, date) VALUES ("Salmon burger", true, "2017-09-05 16:20:16");
